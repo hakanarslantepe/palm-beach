b5c499072da6d7b6f64b9d681f0d61473e47c1da
Hakan Arslantepe <hakanarslantepee@gmail.com>
2023-08-23 00:46:56 +0300

    guncelleme

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -3,14 +3,14 @@ import Image from "../../../node_modules/next/image";
 
 const Gallery = () => {
   return (
-    <div className="mt-10 flex flex-col">
+    <div className="mt-10 flex flex-col max-w-[1440px]">
       <div>
         <Image src="/gallery-top.png" alt="" width={1440} height={192} />
       </div>
-      <div className="mt-10 flex justify-around">
-      <Image src="/gallery-img1.png" alt="" width={426} height={424} />
-      <Image src="/gallery-img2.png" alt="" width={426} height={424} />
-      <Image src="/gallery-img3.png" alt="" width={426} height={424} />
+      <div className="mt-10 flex overflow-x-scroll justify-around gap-x-2">
+        <Image src="/gallery-img1.png" alt="" width={426} height={424} />
+        <Image src="/gallery-img2.png" alt="" width={426} height={424} />
+        <Image src="/gallery-img3.png" alt="" width={426} height={424} />
       </div>
       <div className="mt-10">
         <Image src="/gallery-bottom.png" alt="" width={1440} height={192} />
